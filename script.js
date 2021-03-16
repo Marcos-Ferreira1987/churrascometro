f1217fb0acd20f507cb4ac794b4f83d6340f31c4
Marcos-Ferreira1987 <ferreiramarcola5@gmail.com>
2021-03-15 21:19:50 -0300

    Churrascometro

---
+++ script.js
@@ -0,0 +1,52 @@
+// Carne - 400gr por pessoa   + 6 de horas  650gr
+// Cerveja - 1200ml por pessoa   + 6 horas 2000ml
+// Refrigerante/agua - 1000ml por pessoa   + 6 horas 1500ml
+
+// crianças valem por 0,5
+
+let inputAdultos = document.getElementById("adultos");
+let inputCriancas = document.getElementById("criancas");
+let inputDuracao = document.getElementById("duracao");
+
+let resultado = document.getElementById("resultado");
+
+function calcular(){
+    console.log("Calculando...")
+
+    let adultos = inputAdultos.value;
+    let criancas = inputCriancas.value;
+    let duracao = inputDuracao.value;
+
+    let qdtTotalCarne = carnePP(duracao) * adultos + (carnePP(duracao) / 2 * criancas);
+    let qdtTotalCerveja = cervejaPP(duracao) * adultos;
+    let qdtTotalBebidas = bebidasPP(duracao) * adultos + (bebidasPP(duracao) / 2 * criancas);
+
+    resultado.innerHTML = `<p>${qdtTotalCarne / 1000} kg de carne</p>`
+    resultado.innerHTML += `<p>${Math.ceil(qdtTotalCerveja / 355)} latas de cerveja</p>`
+    resultado.innerHTML += `<p>${Math.ceil(qdtTotalBebidas/2000)} pet's de 2l de bebidas</p>`
+
+}
+
+function carnePP(duracao) {
+    if (duracao >= 6) {
+        return 650;
+    } else {
+        return 400;
+    }
+}
+
+function cervejaPP(duracao) {
+    if (duracao >= 6) {
+        return 2000;
+    } else {
+        return 1200;
+    }
+}
+
+function bebidasPP(duracao) {
+    if (duracao >= 6) {
+        return 1500;
+    } else {
+        return 1000;
+    }
+}
